963eba2ae2eb2e4ef4f532c028ae4f8323dda345
Borys <chabiorborys@gmail.com>
2020-10-07 17:43:54 +0200

    Django blog project

---
+++ posts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'github.html')
