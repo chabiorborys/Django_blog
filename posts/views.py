eaca765019f8fb8323fae33fdf845215c8c5d12b
Borys <chabiorborys@gmail.com>
2020-10-07 18:13:05 +0200

    moved requirements to src.

--- posts/views.py
+++ posts/views.py
@@ -4,4 +4,4 @@ from django.views import View
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'github.html')
+        return render(request, 'github.html'{})
